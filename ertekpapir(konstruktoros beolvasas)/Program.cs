f0ebfca457dfcc26779c75d36dbdf1478862252d
robertfromthemountain <robi20010713@gmail.com>
2022-10-17 11:47:49 +0200

    Add project files.

---
+++ ertekpapir(konstruktoros beolvasas)/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ertekpapir_konstruktoros_beolvasas_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Elore megadott meretu (fajlban szereplo sorok szama) ertekpapir tipusu tomb letrehozasa
+            ertekpapir[] ertekpapirs = new ertekpapir[5];
+
+            try
+            {
+                // Fajlbeolvasas
+                StreamReader read = new StreamReader("teszt.txt");
+                for (int i = 0; i < ertekpapirs.Length; i++)
+                {
+                    // Olvassa be a fajl sorait ugy, hogy a sorokbol letrehozott Ertekpapir peldanyokat irja bele egy 5 elemu tombbe  
+                    ertekpapirs[i] = new ertekpapir(read.ReadLine());
+                }
+                // Olvasas bezarasa
+                read.Close();
+            }
+            catch (Exception e)
+            {
+                // Fajl megnyitasa soran adodo kivetel kezelese, Message tag kiirasaval
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            // Kiiratas Kiir() fuggvennyel:
+            Console.WriteLine();
+            Console.WriteLine("Ertekpapirok adatokkal: ");
+            for (int i = 0; i < ertekpapirs.Length; i++)
+            {
+                ertekpapirs[i].Kiir();
+            }
+
+            // Kiiratas ToString() fuggvennyel:
+            Console.WriteLine();
+            Console.WriteLine("Ertekpapirok hozamokkal: ");
+            for (int i = 0; i < ertekpapirs.Length; i++)
+            {
+                Console.WriteLine(ertekpapirs[i]);
+            }
+
+            // Hozzon letre egy Sikeresek nevu ertekpapir tombot, mely azokat az ertekpapir peldanyokat gyujti, melyek hozamerteke pozitiv
+            ertekpapir[] sikeresek = new ertekpapir[5];
+            int seged = 0;
+            for (int i = 0; i < sikeresek.Length; i++)
+            {
+                if (ertekpapirs[i].Hozam() > 0)
+                {
+                    sikeresek[seged] = ertekpapirs[i];
+                    seged++;
+                }
+            }
+
+            // Sikeresek kiiratasa:
+            Console.WriteLine();
+            Console.WriteLine("Sikeres ertekpapirok (hozam>0):");
+            foreach (var item in sikeresek)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ ertekpapir(konstruktoros beolvasas)/ertekpapir.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ertekpapir_konstruktoros_beolvasas_
+{
+    // Definialjon egy ertekpapir nevu osztalyt melynek tagjai,
+    internal class ertekpapir
+    {
+        // Nev, ertekpapir neve
+        private string nev;
+        // Eredmenyek, egy lebegopontos szamokat tarolo tomb-valtozo.
+        private double[] eredmenyek;
+
+        //Parameteres konstruktor, mely parameterkent egy stringben veszi at az ertekpapir adatait
+        public ertekpapir(string s)
+        {
+            // Splittel whitespaceknel darabokra bontjuk a sort egy tombbe
+            string[] darabok = s.Split(' ');
+            // Letrehozzuk az eredmenyek tombot akkora mereture ahany ertek szerepel a sorban (-1 azert kell mert a 0. ertek a nev)
+            eredmenyek = new double[darabok.Length - 1];
+            // A 0. ertek az ertekpapir neve
+            nev = darabok[0];
+
+            for (int i = 0; i < eredmenyek.Length; i++)
+            {
+                try
+                {
+                    // Az ertekeket beirjuk az eredmenyek tombbe "darab[i+1]" azert kell, mert a 0. elem a név, a for pedig 0-tol indul
+                    eredmenyek[i] = double.Parse(darabok[i + 1]);
+                }
+                // A konvertalals soran adodod kivetelt ugy kezeljuk, hogy a helyere egy 0-t irunk a tombbe
+                catch (Exception)
+                {
+                    eredmenyek[i] = 0;
+                    // Kiiratjuk a kivetel pontos helyet
+                    Console.WriteLine(nev + "ertekpapir " + (i + 1) + ". eleme rossz!");
+
+                }
+            }
+
+        }
+
+        // Hozam nevu metodus, melynek visszateresi erteke hozam tipusu, es a szamok osszeget hozza vissza:
+        public double Hozam()
+        {
+            return eredmenyek.Sum();
+        }
+
+        // Object ToString ujradefinialasa ugy, hogy az ertekpapir neve es a Hozam() fuggveny kerul kiirasra:
+        public override string ToString()
+        {
+            return nev + " " + Hozam();
+        }
+
+        // Kiir() fuggveny, mely kiirja az osszes adatot az ertekpapirrol:
+        public void Kiir()
+        {
+            Console.Write(nev + " ");
+            foreach (var item in eredmenyek)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
